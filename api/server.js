297b97a3451fdd29ac6edd67316b29b422eb0e23
Andre_Johnson <Andrejohnson513@gmail.com>
2021-12-03 00:36:26 -0500

    asegf

--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+const postRouter = require('./posts/posts-router')
+
+server.use(express.json)
+
+server.use('/api/posts', postRouter)
+
+server.use('*', (req, res) =>{
+  res.status(404).json({
+    message: 'not fond'
+  })
+})
+
+module.exports = server;
